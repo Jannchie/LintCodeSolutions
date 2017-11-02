f26287a67c023d4448723fae0086d94102908987
jannchie <jannchie@gmail.com>
2017-11-02 23:58:30 +0800

    commit

--- longestWords.py
+++ longestWords.py
@@ -3,13 +3,30 @@ class Solution:
     @param: dictionary: an array of strings
     @return: an arraylist of strings
     """
+
     def longestWords(self, dictionary):
         maxlen = 0
-        arraylist = list()   
+        arraylist = list()
         for word in dictionary:
             if maxlen < len(word):
                 maxlen = len(word)
         for word in dictionary:
-            if len(word)==maxlen:
+            if len(word) == maxlen:
                 arraylist.append(word)
         return arraylist
+
+    def longestWords_2(self, dictionary):
+        maxlen = 0
+        arraylist = list()
+        for word in dictionary:
+            if maxlen < len(word):
+                maxlen = len(word)
+                arraylist = list()
+            if maxlen == len(word):
+                arraylist.append(word)
+        return arraylist
+
+
+s = Solution()
+s.longestWords_2(
+    ["dog", "google", "facebook", "internationalization", "blabla"])
