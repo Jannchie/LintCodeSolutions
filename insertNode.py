42a1e5d669b2d74c2d66080224ae59e193e2ce2c
jannchie <jannchie@gmail.com>
2017-11-02 23:59:56 +0800

    other

--- insertNode.py
+++ insertNode.py
@@ -1,9 +1,7 @@
-
 class TreeNode:
     def __init__(self, val):
         self.val = val
         self.left, self.right = None, None
-
 
 
 class Solution:
@@ -12,6 +10,7 @@ class Solution:
     @param: node: insert this node into the binary search tree
     @return: The root of the new binary search tree.
     """
+
     def insertNode(self, root, node):
         # write your code here
         if root == None:
@@ -19,16 +18,17 @@ class Solution:
         if node.val < root.val:
             if root.left == None:
                 root.left = node
-            self.insertNode(root.left,node)
+            self.insertNode(root.left, node)
         if node.val > root.val:
             if root.right == None:
                 root.right = node
-            self.insertNode(root.right,node)
+            self.insertNode(root.right, node)
         return root
+
 
 s = Solution()
 a = TreeNode(2)
 b = TreeNode(1)
 c = TreeNode(3)
 a.left = b
-s.insertNode(a,c)
+s.insertNode(a, c)
